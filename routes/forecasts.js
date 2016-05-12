d1fbc2b2aed2aa030c3a8e9a658e904d15a9aa54
TarikFojnica <tarik.f786@gmail.com>
2016-05-12 19:20:11 +0200

    Update

--- routes/forecasts.js
+++ routes/forecasts.js
@@ -1,55 +1,44 @@
 var express = require('express');
 var router = express.Router();
-
+var moment = require('moment');
 var ForecastIo = require('forecastio');
 var forecastIo = new ForecastIo('d8ab77870812de67277ae47d3e9bf83e');
 
+var last30Dates = [];
+var forecasts = [];
+
+
+function getLast30Dates(callback) {
+	for (var i = 0; i <= 29; i++) {
+		//taking unix time of each day in the last 30 days and storing it in the 'last30Dates' array
+		last30Dates.push(Math.floor((moment().subtract(i, 'days')) / 1000));
+		console.log("we have all data");
+	}
+
+	callback();
+}
+
+
+router.get('/past-days/', function(req, res) {
+	getLast30Dates( function(){
+		for (var i = 0; i <= 29; i++) {
+			forecastIo.forecast(req.query.lat, req.query.lng, last30Dates[i]).then(function(data) {
+				forecasts.push(data)
+			});
+
+			if (i >= 29) {
+				res.json(forecasts);
+			}
+		}
+	});
+});
 
 router.get('/', function(req, res) {
+	getLast30Dates();
 	forecastIo.forecast(req.query.lat, req.query.lng).then(function(data) {
 		res.json(data);
 	});
 });
 
-
-router.get('/past-days/', function(req, res) {
-	forecastIo.forecast(req.query.lat, req.query.lng, req.query.date).then(function(data) {
-		res.json(data);
-	});
-});
-
-/* GET users listing. */
-//router.get('/', function(req, res) {
-//  //Lets configure and request
-//  request({
-//    url: 'https://api.forecast.io/forecast/' + api + req.query.lat + ',' + req.query.lng,
-//    method: 'GET',
-//    headers: {
-//      'Content-Type': 'application/json'
-//    }
-//
-//  }, function(error, response, body){
-//    if(error) {
-//      console.log(error);
-//    } else {
-//      console.log(response.statusCode, body);
-//      res.json(JSON.parse(body));
-//    }
-//  });
-//});
-//
-//router.get('/past-days/', function(req, res) {
-//  //Lets configure and request
-//  request({
-//    url: 'https://api.forecast.io/forecast/'+ api + req.query.lat + ',' + req.query.lng + ',' + req.query.date
-//  }, function(error, response, body){
-//    if(error) {
-//      console.log(error);
-//    } else {
-//      console.log(response.statusCode, body);
-//      res.json(JSON.parse(body));
-//    }
-//  });
-//});
 
 module.exports = router;
