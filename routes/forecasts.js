1b1ee97159cbc6bd478b9423c0dbd7ae3755fcd0
TarikFojnica <tarik.f786@gmail.com>
2016-05-12 14:14:23 +0200

    Add new features

--- routes/forecasts.js
+++ routes/forecasts.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
+var api = 'd8ab77870812de67277ae47d3e9bf83e/';
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   //Lets configure and request
   request({
-    url: 'https://api.forecast.io/forecast/d8ab77870812de67277ae47d3e9bf83e/'+ req.query.lat + req.query.lng,
+    url: 'https://api.forecast.io/forecast/' + api + req.query.lat + ',' + req.query.lng,
     method: 'GET',
-    headers: {
-      'Content-Type': 'MyContentType',
-      'Custom-Header': 'Custom Value'
-    }
   }, function(error, response, body){
     if(error) {
       console.log(error);
     } else {
       console.log(response.statusCode, body);
-      res.send(req.query.lat + ' , ' + req.query.lng);
+      res.json(body);
     }
   });
-})
+});
 
+router.get('/past-days/', function(req, res) {
+  //Lets configure and request
+  request({
+    url: 'https://api.forecast.io/forecast/'+ api + req.query.lat + ',' + req.query.lng + ',' + req.query.date,
+  }, function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode, body);
+      res.json(body);
+    }
+  });
+});
 
 module.exports = router;
